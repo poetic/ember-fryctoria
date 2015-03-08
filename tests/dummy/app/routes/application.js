3f4e1afc6e6f7713b455cf570f9cd455c9788fae
Chun Yang <trent@poeticsystems.com>
2015-03-08 15:05:19 -0500

    [feature] add a button to toggle online and offline

---
+++ tests/dummy/app/routes/application.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+import ENV   from 'dummy/config/environment';
+
+export default Ember.Route.extend({
+  setupController: function(ctrl) {
+    ctrl.set('isOnline', true);
+  },
+
+  actions: {
+    toggleOnline: function() {
+      var ctrl = this.get('controller');
+      var isOnline = ctrl.get('isOnline');
+
+      if(isOnline) {
+        var mockId = Ember.$.mockjax({
+          status: 0,
+          url: /.*/g,
+        });
+        ctrl.set('mockId', mockId);
+      } else {
+        Ember.$.mockjax.clear(ctrl.get('mockId'));
+      }
+
+      ctrl.set('isOnline', !isOnline);
+    }
+  }
+});
+
+
+
+
