e56fb0c0480ff20159de1705913b7292fed9ec35
Chun Yang <trent@poeticsystems.com>
2015-03-21 15:46:02 -0500

    [upgrade] ember cli 0.2.0

---
+++ tests/acceptance/fetch-all-test.js
@@ -0,0 +1,30 @@
+/* jshint expr:true */
+import {
+  describe,
+  it,
+  beforeEach,
+  afterEach
+} from 'mocha';
+import { expect } from 'chai';
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+
+var App;
+
+describe('Acceptance: FetchAll', function() {
+  beforeEach(function() {
+    App = startApp();
+  });
+
+  afterEach(function() {
+    Ember.run(App, 'destroy');
+  });
+
+  it('can visit /fetch-all', function() {
+    visit('/fetch-all');
+
+    andThen(function() {
+      expect(currentPath()).to.equal('fetch-all');
+    });
+  });
+});
